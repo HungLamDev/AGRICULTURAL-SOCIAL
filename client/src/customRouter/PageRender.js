f1343bb0cdf374a38f62829c8c3d552b2622db28
khoa <doandangkhoa0711.gr@gmail.com>
2024-08-20 22:39:10 +0700

    khoa

--- client/src/customRouter/PageRender.js
+++ client/src/customRouter/PageRender.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import {useParams} from 'react-router-dom'
-import NotFound from './components/NotFound'
+import NotFound from '../components/NotFound'
+import { useSelector } from 'react-redux'
+
 const generatePage = (pageName) => {
-  const element = () => require(`./pages/${pageName}`).default
+  const element = () => require(`../pages/${pageName}`).default
   try{
     return React.createElement(element())
   } catch(err){
@@ -11,13 +13,15 @@ const generatePage = (pageName) => {
 }
 const PageRender = () => {
   const {page, id} = useParams()
+  const {auth} = useSelector(state => state)
   let pageName = "";
-  if(id) {
-    pageName = `${page}/[id]`
-  }else{
-    pageName = `${page}`
-  } 
-  
+  if(auth.token){
+    if(id) {
+      pageName = `${page}/[id]`
+    }else{
+      pageName = `${page}`
+    } 
+  }
   return generatePage(pageName)
 }
 
