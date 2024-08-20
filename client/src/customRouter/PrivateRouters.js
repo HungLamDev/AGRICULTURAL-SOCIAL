74eebe828f51730702e33f37a661af01d1283371
HungLam <110954643+HungLamDev@users.noreply.github.com>
2024-08-20 22:52:11 +0700

    Revert "#1 search (hưng)"

--- client/src/customRouter/PrivateRouters.js
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRouter = ({ element: Element, ...rest }) => {
-  const firstLogin = localStorage.getItem("firstLogin");
-
-  return firstLogin ? <Element {...rest} /> : <Navigate to="/" />;
-};
-
-export default PrivateRouter;
--- client/src/pages/discover.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Discover = () => {
-  return <div>Dicover</div>;
-};
-
-export default Discover;
