f0a29b53c3bd7078ce510f1c8c37152a7a721c1c
HungLam <110954643+HungLamDev@users.noreply.github.com>
2024-08-26 10:28:54 +0700

    Revert "#2 fix route edit profile"

--- client/src/redux/actions/globalTypes.js
+++ client/src/redux/actions/globalTypes.js
@@ -1,20 +1,5 @@
 export const GLOBALTYPES = {
-  AUTH: "AUTH",
-  ALERT: "ALERT",
-  THEME: "THEME",
-  MODE: "MODE",
-  STATUS: "STATUS",
-  SOCKET: "SOCKET",
-  ONLINE: "ONLINE",
-  OFFLINE: "OFFLINE",
-};
-
-export const EditData = (data, id, post) => {
-  const newData = data.map((item) => (item._id === id ? post : item));
-  return newData;
-};
-
-export const DeleteData = (data, id) => {
-  const newData = data.filter((item) => item._id !== id);
-  return newData;
-};
+      AUTH: 'AUTH',
+      ALERT: 'ALERT',
+      THEME: 'THEME'
+}
--- client/src/redux/actions/profileAction.js
+++ client/src/redux/actions/profileAction.js
@@ -1,85 +1,28 @@
 import { GLOBALTYPES } from "./globalTypes";
-import { getDataAPI, putDataAPI } from "../../utils/fetchData";
-import { imageUpload } from "../../utils/imageUpload";
+import { getDataAPI } from "../../utils/fetchData";
 export const PROFILE_TYPES = {
-  LOADING: "LOADING",
-  GET_USER: "GET_USER",
+  LOADING: 'LOADING',
+  GET_USER: 'GET_USER',
 };
 
-export const getProfileUsers =
-  ({ users = [], id, auth }) =>
-  async (dispatch) => {
-    if (!users || !users.find((user) => user._id === id)) {
-      try {
-        dispatch({ type: PROFILE_TYPES.LOADING, payload: true });
-        const res = await getDataAPI(`/user/${id}`, auth.token);
-        console.log(res);
-
-        dispatch({
-          type: PROFILE_TYPES.GET_USER,
-          payload: { user: res.data.user },
-        });
-
-        dispatch({ type: PROFILE_TYPES.LOADING, payload: false });
-      } catch (err) {
-        dispatch({
-          type: GLOBALTYPES.ALERT,
-          payload: { error: err.response?.data?.msg || "An error occurred" },
-        });
-      }
-    }
-  };
-export const updateUserProfile =
-  ({ userData, avatar, auth }) =>
-  async (dispatch) => {
-    if (!userData.fullname)
-      return dispatch({
-        type: GLOBALTYPES.ALERT,
-        payload: { err: "Nhập tên đăng nhập !" },
-      });
-    if (userData.fullname.length > 25)
-      return dispatch({
-        type: GLOBALTYPES.ALERT,
-        payload: { err: "Tên đăng nhập quá dài !" },
-      });
-    if (userData.story.length > 200)
-      return dispatch({
-        type: GLOBALTYPES.ALERT,
-        payload: { err: "Thông tin quá dài !" },
-      });
+export const getProfileUsers = ({ users = [], id, auth }) => async (dispatch) => {
+  if (!users || !users.find(user => user._id === id)) {
     try {
-      let media;
-      dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: true } });
-      if (avatar) media = await imageUpload([avatar]);
-      console.log(media);
-      const res = await putDataAPI(
-        `/user/${auth.user._id}`,
-        {
-          ...userData,
-          profilePicture: avatar ? media[0].url : auth.user.avatar,
-        },
-        auth.token
-      );
+      dispatch({ type: PROFILE_TYPES.LOADING, payload: true });
+      const res = await getDataAPI(`/user/${id}`, auth.token);
       console.log(res);
+      
       dispatch({
-        type: GLOBALTYPES.AUTH,
-        payload: {
-          ...auth,
-          user: {
-            ...auth.user,
-            ...userData,
-            profilePicture: avatar ? media[0].url : auth.user.avatar,
-          },
-        },
+        type: PROFILE_TYPES.GET_USER,
+        payload: { user: res.data.user }
       });
-      dispatch({
-        type: GLOBALTYPES.ALERT,
-        payload: { err: res.response?.data?.msg || "Đã xảy ra lỗi" },
-      });
+
+      dispatch({ type: PROFILE_TYPES.LOADING, payload: false });
     } catch (err) {
       dispatch({
-        type: GLOBALTYPES.ALERT,
-        payload: { err: err.response?.data?.msg || "Đã xảy ra lỗi" },
+        type: GLOBALTYPES.ALERT, 
+        payload: { error: err.response?.data?.msg || 'An error occurred' }
       });
     }
-  };
+  }
+};
