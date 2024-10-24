f467b0313e0439937f0c04c91e3c61cee720fffc
HungLam <110954643+HungLamDev@users.noreply.github.com>
2024-10-24 10:04:00 +0700

    login admin

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,39 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { refreshToken } from "./redux/actions/authAction";
+
+import PageRender from "./customRouter/RoutePage";
+import LoginPage from "./pages/login";
+import HomePage from "./pages/home";
+
+import Header from "./component/header/Header";
+import Load from "./component/Load";
+
 function App() {
+  const auth = useSelector((state) => state.auth.token);
+  const loading = useSelector((state) => state.notify?.loading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshToken());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="main">
-        <h1>admin</h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="main">
+          {auth && <Header />}
+          {loading && <Load />}
+          <Routes>
+            <Route path="/" element={auth ? <HomePage /> : <LoginPage />} />
+            <Route exact path="/:page" element={<PageRender />} />
+            <Route exact path="/:page/:id" element={<PageRender />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
