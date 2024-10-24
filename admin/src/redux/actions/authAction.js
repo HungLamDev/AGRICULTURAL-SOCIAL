e3e54bf85c8fe1339a1ac990d1c9dd308a8b70b4
HungLam <110954643+HungLamDev@users.noreply.github.com>
2024-10-24 11:04:22 +0700

    admin login

--- admin/src/redux/actions/authAction.js
+++ admin/src/redux/actions/authAction.js
@@ -1,64 +1,74 @@
 import { GLOBALTYPES } from "./globalTyle";
-import { postDataAPI } from "../../untils/fetchData";
+import { postDataAPI } from "../../unitils/fetchData";
 
 export const login = (data) => async (dispatch) => {
   try {
     dispatch({ type: GLOBALTYPES.NOTIFY, payload: { loading: true } });
-    const res = await postDataAPI("/auth/login", data);
-    dispatch({
-      type: GLOBALTYPES.AUTH,
-      payload: {
-        token: res.data.accessToken,
-        user: res.data,
-      }
-    })
-    localStorage.setItem("firstLogin", true)
-    dispatch({ type: GLOBALTYPES.NOTIFY, payload: { loading: false } });
+    const res = await postDataAPI("login", data);
 
+    if (res.data.user.admin === true) {
+      dispatch({
+        type: GLOBALTYPES.AUTH,
+        payload: {
+          token: res.data.access_token,
+          user: res.data.user,
+        },
+      });
+      localStorage.setItem("firstLogin", true);
+      dispatch({
+        type: GLOBALTYPES.NOTIFY,
+        payload: { success: res.data.msg },
+      });
+      window.location.href = "/users";
+    } else {
+      dispatch({
+        type: GLOBALTYPES.NOTIFY,
+        payload: { err: "Bạn không có quyền truy cập trang này!" },
+      });
+    }
   } catch (err) {
+    const errorMsg = err.response?.data?.msg || "Logout failed!";
     dispatch({
       type: GLOBALTYPES.NOTIFY,
-      payload: {
-        err: err.response.data.msg
-      }
-    })
+      payload: { err: errorMsg },
+    });
   }
 };
-export const rfToken = () => async (dispatch) => {
+
+export const refreshToken = () => async (dispatch) => {
   const firstLogin = localStorage.getItem("firstLogin");
   if (firstLogin) {
     dispatch({ type: GLOBALTYPES.NOTIFY, payload: { loading: true } });
     try {
-      const res = await postDataAPI("/auth/refresh");
+      const res = await postDataAPI("refresh_token");
+      console.log(res);
       dispatch({
         type: GLOBALTYPES.AUTH,
         payload: {
-          token: res.data.accessToken,
-          user: res.data
-        }
-      })
-      dispatch({ type: GLOBALTYPES.NOTIFY, payload: {} });
+          token: res.data.access_token,
+          user: res.data.user,
+        },
+      });
+      dispatch({ type: GLOBALTYPES.NOTIFY, payload: { loading: false } });
     } catch (err) {
+      const errorMsg = err.response?.data?.msg || "Logout failed!";
       dispatch({
         type: GLOBALTYPES.NOTIFY,
-        payload: {
-          err: err.response.data.msg
-        }
-      })
+        payload: { err: errorMsg },
+      });
     }
   }
-}
+};
 export const logout = () => async (dispatch) => {
   try {
-    localStorage.removeItem("firstLogin")
-    await postDataAPI("/auth/logout")
-    window.location.href = "/"
+    localStorage.removeItem("firstLogin");
+    await postDataAPI("logout");
+    window.location.href = "/";
   } catch (err) {
+    const errorMsg = err.response?.data?.msg || "Logout failed!";
     dispatch({
-      type: "NOTIFY",
-      payload: {
-        err: err.response.data.msg
-      }
-    })
+      type: GLOBALTYPES.NOTIFY,
+      payload: { err: errorMsg },
+    });
   }
-}
+};
